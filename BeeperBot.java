eef24675f6a3fb69af4a78d2c40a67ad480d8ae2
anicazulch <31353283+anicazulch@users.noreply.github.com>
2017-09-11 11:56:54 -0700

    Lab 4 Day 1

--- BeeperBot.java
+++ BeeperBot.java
@@ -12,7 +12,41 @@ public class BeeperBot extends Robot
     }
     
     public void runRace() {
-        
+        while(frontIsClear()) {
+            collectBeeperSteeple();
+        }
     }
-   
+    public void collectBeeperSteeple() {
+        findBeepers();
+    }
+    public void findBeepers() {
+        faceEast();
+        while(!nextToABeeper()){
+           move(); 
+        }
+        while(nextToABeeper()){
+            for(int x = 0; x<4; x++) {
+            pickBeeper();
+            faceNorth();
+            move();
+        }
+        }
+        faceSouth();
+    }
+    public void faceEast() {
+        while(!facingEast()){
+            turnLeft();
+        }
+    }
+        public void faceSouth() {
+        while(!facingSouth()){
+            turnLeft();
+        }
+    }
+    public void faceNorth() {
+        while(!facingNorth()) {
+            turnLeft();
+        }
+    }
+    
 }
